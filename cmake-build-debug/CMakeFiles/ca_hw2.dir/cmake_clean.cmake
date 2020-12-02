96d724570463b381da95b74a2871a0cee2bbe1d9
Nadav Ashkenazi <nadavash@campus.technion.ac.il>
2020-12-02 16:21:28 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/ca_hw2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ca_hw2.dir/main.cpp.o"
+  "ca_hw2"
+  "ca_hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ca_hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ca_hw2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ca_hw2.
+# This may be replaced when dependencies are built.
